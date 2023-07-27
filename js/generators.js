dfdabbdbc00394d9bdeafafe648c47e626a2c72a
frinkifail <frinkifail@cat.cat>
2023-07-27 23:57:31 +0700

    move

--- js/generators.js
+++
@@ -1,6 +0,0 @@
-const gen1button = document.getElementById("generator1 button")
-
-gen1button.onclick = function(){
-    if (data.simulons >= data.generators[1].cost) {data.simulons -= data.generators[1].cost;data.generators[1].amount += 1;
-        data.generators[1].boost += 0.01;} else {}
-}
--- js/reactor.js
+++
@@ -1,2 +0,0 @@
-// Reactors
-// :pain:
