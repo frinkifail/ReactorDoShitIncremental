da166f7d2e9556f58a34002a6bf29bdc46198e60
frinkifail <frinkifail@cat.cat>
2023-07-28 00:54:50 +0700

    buy max

--- src/generators.ts
+++ src/generators.ts
@@ -2,11 +2,17 @@ const generators = {
     g_tier: _typesafe.e('gen.g')
 }
 
-function buy_generator(index: number) {
-    const gen = data.generators[index]
-    data.simulons -= gen.cost
-    gen.cost *= gen.boost
-    gen.amount += 1
+function buy_generator(index: number): boolean {
+    if (available(index)) {
+        const gen = data.generators[index]
+        data.simulons -= gen.cost
+        gen.cost *= gen.boost
+        gen.amount += 1
+        return true
+    } else {
+        console.info("> Skill Issue")
+        return false
+    }
 }
 function calculate_income() {
     data.income = 0
@@ -16,9 +22,11 @@ function calculate_income() {
 }
 
 generators.g_tier.onclick = () => {
-    if (available(GeneratorIndex.g_tier)) {
+    buy_generator(GeneratorIndex.g_tier)
+}
+
+_typesafe.e('gen.max').onclick = () => {
+    while (available(GeneratorIndex.g_tier)) {
         buy_generator(GeneratorIndex.g_tier)
-    } else {
-        console.info("> Skill Issue")
     }
 }
